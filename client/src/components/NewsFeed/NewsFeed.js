de0ce6bc3be457292f309efa4da64aa63058d1ff
Joseph Kim <josepheh.kim@gmail.com>
2019-08-20 17:09:02 -0400

    Frontend Routing

---
+++ client/src/components/NewsFeed/NewsFeed.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class NewsFeed extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>NewsFeed</h1>
+      </>
+    );
+  }
+}
+export default NewsFeed;
